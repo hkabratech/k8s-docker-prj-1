9ac201e73d5305a62f65cbd99dfb1cd4a138ae48
hkabratech <hkabra.tech@gmail.com>
2025-03-24 11:54:28 -0400

    backend

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        host=os.getenv("POSTGRES_HOST"),
+        port=os.getenv("POSTGRES_PORT")
+    )
+    return conn
+
+@app.route('/health')
+def health():
+    return jsonify({"status": "OK"})
+
+@app.route('/users')
+def users():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM users;')
+    users = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(users)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
